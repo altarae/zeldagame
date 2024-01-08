61b5b9ae32d161658c4c3c581f89d50704b18019
alt <itsdezyre@gmail.com>
2024-01-08 17:35:36 +0000

    + Enemy AI, Breakable Pots, Attack Hitboxes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,6 +21,8 @@ public class PlayerMovement : MonoBehaviour
         currentState = PlayerState.walk;
         animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
     }
 
     // Update is called once per frame
@@ -62,6 +64,7 @@ public class PlayerMovement : MonoBehaviour
     }
     void MoveCharacter()
     {
+        change.Normalize();
         myRigidbody.MovePosition(
             transform.position + change * speed * Time.deltaTime
         );
