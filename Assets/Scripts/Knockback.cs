e678ae4654b636228fb35a2a9b768ad622443be0
alt <itsdezyre@gmail.com>
2024-01-12 03:40:08 +0000

    end of day commit + health signals / graphics

--- Assets/Scripts/Knockback.cs
+++ Assets/Scripts/Knockback.cs
@@ -30,8 +30,11 @@ public class Knockback : MonoBehaviour
                 }
                 if(other.gameObject.CompareTag("Player"))
                 {
-                    hit.GetComponent<PlayerMovement>().currentState = PlayerState.stagger;
-                    other.GetComponent<PlayerMovement>().Knock(knockTime);
+                    if(other.GetComponent<PlayerMovement>().currentState != PlayerState.stagger)
+                    {
+                        hit.GetComponent<PlayerMovement>().currentState = PlayerState.stagger;
+                        other.GetComponent<PlayerMovement>().Knock(knockTime, damage);
+                    }
                 }
             }
         }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,6 +17,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D myRigidbody;
     private Vector3 change;
     private Animator animator;
+    public FloatValue currentHealth;
+    public SignalGame playerHealthSignal;
 
     // Start is called before the first frame update
     void Start() {
@@ -72,9 +74,14 @@ public class PlayerMovement : MonoBehaviour
         );
     }
 
-    public void Knock(float knockTime)
+    public void Knock(float knockTime, float damage)
     {
-        StartCoroutine(KnockCo(knockTime));
+        currentHealth.initialValue -= damage;
+        if(currentHealth.initialValue > 0)
+        {
+            playerHealthSignal.Raise();
+            StartCoroutine(KnockCo(knockTime));
+        }
     }
 
     private IEnumerator KnockCo(float knockTime)
